adae402d60db55b064e406b4122f7920271eb5c5
LidiaCana <in.canaless@gmail.com>
2025-03-17 12:37:43 -0400

    style: import dashboard style

--- tab/src/views/dashboard/dashboard.tsx
+++ tab/src/views/dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ import { Divider, Title1 } from "@fluentui/react-components";
 import FilterBar from "../../components/common/filterBar";
 import data from "../../mockAPI/kpi.json";
 import Card from "../../components/common/card";
-
+import "./dashboard.css";
 const Dashboard = () => {
   const [items, setItems] = useState(data);
   useEffect(() => {
@@ -16,7 +16,7 @@ const Dashboard = () => {
         onSearch={(x) => console.log(x)}
         onFilterChange={(y) => console.log(y)}
       />
-      <div>
+      <div className="card-container-wrapper">
         {items.map(({ id, name, url, goal, actual }) => (
           <Card title={name} url={url} goal={goal} actual={actual} />
         ))}
